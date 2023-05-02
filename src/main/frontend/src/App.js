695457fe9b3602504e227532ec77d1ee4c6ecb9a
0exia099 <97746056+0exia099@users.noreply.github.com>
2023-05-03 02:48:58 +0900

    App.js 수정
    홈, 로그인, 회원가입 페이지 추가(미완성)

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useState} from 'react';
 import CommonTable from './Tables/CommonTable';
 import CommonTableColumn from './Tables/CommonTableColumn';
 import CommonTableRow from './Tables/CommonTableRow';
-import {Routes,Route,Link,NavLink,useNavigate} from 'react-router-dom';
+import {Routes, Route, Link, NavLink, useNavigate} from 'react-router-dom';
 import axios from 'axios';
+import LogInPage from './pages/login_page';
+import MemPage from './pages/joinmem_page';
+import Home from './pages/home';
 import OtherPage from "./pages/OtherPage";
 import OtherPage2 from "./pages/OtherPage2";
 import BoughtList from "./pages/BoughtList";
@@ -13,21 +15,19 @@ import FixBar from "./pages/FixBar";
 
 
 function App() {
-
-  return (
-      <div id='root'>
-
-
-          <Routes>
-              <Route path="/pages/OtherPage" exact={true} element={<OtherPage />}/>
-              <Route path="/pages/OtherPage2" exact={true} element={<OtherPage2 />}/>
-              <Route path="/pages/BoughtList" exact={true} element={<BoughtList />}/>
-              <Route path="/pages/FixBar" exact={true} element={<FixBar />}/>
-          </Routes>
-
-
-      </div>
-  );
+    return (
+        <div id='root'>
+            <Routes>
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="/pages/loginpage" element={<LogInPage/>}></Route>
+                <Route path="/pages/joinmempage" element={<MemPage/>}></Route>
+                <Route path="/pages/OtherPage" exact={true} element={<OtherPage/>}/>
+                <Route path="/pages/OtherPage2" exact={true} element={<OtherPage2/>}/>
+                <Route path="/pages/BoughtList" exact={true} element={<BoughtList/>}/>
+                <Route path="/pages/FixBar" exact={true} element={<FixBar/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
