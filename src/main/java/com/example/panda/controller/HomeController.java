e2f2b647edbbb879d4f317390f06dea861a0e359
diqzk1562 <97776233+diqzk1562@users.noreply.github.com>
2023-05-03 18:44:35 +0900

    채팅 관련 DTO 및 Controller 추가
    채팅 관련 DTO 및 Controller 추가

--- src/main/java/com/example/panda/controller/HomeController.java
+++ src/main/java/com/example/panda/controller/HomeController.java
@@ -1,11 +1,12 @@
 package com.example.panda.controller;
 
-import org.springframework.stereotype.Controller;
+import com.example.panda.dto.MessageDTO;
+import com.example.panda.dto.MessageRoomDTO;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -20,5 +21,33 @@ public class HomeController {
         abc.add("수박");
         abc.add("메론");
         return abc;
+    }
+
+    /*
+    *  MessageRoom (채팅방) DB가 필요할 것 같아서 만듦.
+    *  Message & Message Room DB 설계 참고 자료
+    *  https://youngminz.netlify.app/posts/django-1-to-1-chat-service
+    */
+
+    /* 채팅 테스트용 (0번 채팅방) */
+    @GetMapping("/api/chat")
+    public List<MessageDTO> chatTest() {
+        List<MessageDTO> chatTest = new ArrayList<>();
+        chatTest.add(new MessageDTO(0, 0, true,  "님", new Date()));
+        chatTest.add(new MessageDTO(1, 0, true, "네고 안하면 안삼", new Date()));
+        chatTest.add(new MessageDTO(2, 0, false, "사지마셈", new Date()));
+        chatTest.add(new MessageDTO(3, 0, true, "신고할게요", new Date()));
+        chatTest.add(new MessageDTO(4, 0, false, "?", new Date()));
+        return chatTest;
+    }
+
+    /* 채팅방 테스트용 */
+    @GetMapping("/api/chatList")
+    public List<MessageRoomDTO> chatListTest() {
+        List<MessageRoomDTO> chatListTest = new ArrayList<>();
+        chatListTest.add(new MessageRoomDTO(0,  "네고왕김네고", "나", "?", new Date()));
+        chatListTest.add(new MessageRoomDTO(1,  "사려는사람", "나", "네고 가능?", new Date()));
+
+        return chatListTest;
     }
 }
