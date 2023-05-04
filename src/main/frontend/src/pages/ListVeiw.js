9b85e57f884fac936bc69b5ca2b92af3b8d27234
0exia099 <97746056+0exia099@users.noreply.github.com>
2023-05-04 20:59:57 +0900

    Add files via upload

--- src/main/frontend/src/pages/ListVeiw.js
+++ src/main/frontend/src/pages/ListVeiw.js
@@ -10,16 +10,16 @@ class ListVeiw extends React.Component {
     imgQuantity = this.props.imagesData.length;
     //데이터로 들어오는 총 이미지 수가 항상 다르기 때문에 총 이미지 수를 구해준다.
     slideWidth =
-        IMG_WIDTH * this.imgQuantity + (this.imgQuantity - 1) * SLIDE_GAP;
+        IMG_WIDTH * (this.imgQuantity + 1) + (this.imgQuantity) * SLIDE_GAP * 2;
     //슬라이드 내부 컨텐츠의 전체 길이를 구해준다.
-    hiddenedSlideWidth = this.slideWidth - window.innerWidth;
+    inner_len = window.innerWidth;
+    hiddenedSlideWidth = (this.inner_len < 410 ? this.slideWidth - 300:(this.inner_len < 670 ? this.slideWidth - 500 : this.slideWidth - 700));
     //슬라이드 내부 컨텐츠 전체 길이에서 윈도우의 innerWidth 값을 빼 남아있는 슬라이드의 길이를 구한다.
     slideEnd;
     //슬라이드의 끝부분에 갔을 때 next 버튼이 없어지도록 만들 때 사용할 변수이다.
 
     handlePrevBtn = () => {
         const { slideSpot } = this.state;
-
         if (Math.abs(slideSpot) < SLIDE_MOVING_UNIT) {
             //슬라이드 왼쪽으로 남은 값이 한 번에 이동하는 값보다 작으면
 
@@ -42,7 +42,7 @@ class ListVeiw extends React.Component {
     handleNextBtn = () => {
         const { slideSpot } = this.state;
 
-        if (this.hiddenedSlideWidth - Math.abs(slideSpot) < SLIDE_MOVING_UNIT) {
+        if (this.hiddenedSlideWidth - Math.abs(slideSpot) <= SLIDE_MOVING_UNIT) {
             //남아있는 슬라이드의 길이에서 현재 슬라이드의 위치값을 뺀 값이 한 번에 움직여야 하는 값보다 작으면
 
             this.setState({
@@ -86,7 +86,7 @@ class ListVeiw extends React.Component {
                             <li key={i} className={styles.storeImgLi}>
                                 <div className={styles.list_container}>
                                     <div className={styles.item_img}>
-                                        <img width={300} src={img} className={styles.list_img}/>
+                                        <img width={200} src={img} className={styles.list_img}/>
                                     </div>
                                     <div className={styles.item_desc}>
                                         <h2>{pname[i]}</h2>
@@ -118,10 +118,15 @@ ListVeiw.defaultProps = {
     price : ['000','000','000','000','000','000'],
     addr : ['aaa','aaa','aaa','aaa','aaa','aaa'],
     count : ['100','100','100','100','100','100']
+    // imagesData: ['imgs/logo192_192.png', 'imgs/menu_logo.png', 'imgs/close_logo.png', 'imgs/logo192_192.png', 'imgs/menu_logo.png'],
+    // pname: ['제목','제목','제목','제목','제목'],
+    // price : ['000','000','000','000','000'],
+    // addr : ['aaa','aaa','aaa','aaa','aaa'],
+    // count : ['100','100','100','100','100']
 }
 
 const SLIDE_GAP = 14;  //각 슬라이드 사이 간격
-const SLIDE_MOVING_UNIT = 500;  //슬라이드 버튼 클릭 시 움직일 길이
-const IMG_WIDTH = 400;  //이미지 가로 길이
+const SLIDE_MOVING_UNIT = 228;  //슬라이드 버튼 클릭 시 움직일 길이
+const IMG_WIDTH = 200;  //이미지 가로 길이
 
 export default ListVeiw;
