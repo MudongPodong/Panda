fbaf59640cbd17207bc5c53e12f7a302e6fd22c1
0exia099 <0exia099@gmail.com>
2023-05-05 22:43:05 +0900

    프론트 수정

--- src/main/frontend/src/pages/sidebar.js
+++ src/main/frontend/src/pages/sidebar.js
@@ -57,11 +57,11 @@ const Sidebar = () => {
             </div>
             <div className={(isOpen ? `${styles.show_menu}` : `${styles.hide_menu}`)}>
                 <div className={styles.search_box}>
-                    <form name='search' id='search_form_h' method='get'>
+                    <form name='search' id='search_form_s' method='get'>
                         <input type='text' className={styles.search_input} placeholder='  검색' name='search'></input>
                     </form>
                     <div className={styles.search_btn_wrap}>
-                        <button type='submit' form='search_form_h' className={styles.search_input_btn} onClick={goSearchResult}></button>
+                        <button type='submit' form='search_form_s' className={styles.search_input_btn} onClick={goSearchResult}></button>
                     </div>
                 </div>
                 <div>
