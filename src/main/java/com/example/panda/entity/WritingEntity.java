28103ff79bd417794ef2cb4a34e55087f2acd866
jhng77 <jhng77@naver.com>
2023-05-06 16:18:07 +0900

    Backend Writing테이블 추가

---
+++ src/main/java/com/example/panda/entity/WritingEntity.java
@@ -0,0 +1,35 @@
+package com.example.panda.entity;
+
+import com.example.panda.dto.PhotoDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Writing")
+public class WritingEntity {
+    @Id // pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int wid;
+    @Column(length = 64)
+    private String writing_name;
+    @OneToOne
+    @JoinColumn(name="writing_photo")
+    private PhotoEntity writing_photo;
+    @Column(length = 1024)
+    private String writing_content;
+    @Column(length = 32)
+    private String category;
+    @Column(length = 32)
+    private String detail_category;
+    @Column
+    private Boolean is_sold;
+    @Column
+    private int count;
+    @Column
+    private Boolean is_auction;
+}
