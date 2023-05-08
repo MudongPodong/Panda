2ac562e7347d7931e9e7caf04f58b870bf4aa98a
0exia099 <0exia099@gmail.com>
2023-05-08 17:46:55 +0900

    홈 프론트 수정

--- src/main/frontend/src/pages/home.js
+++ src/main/frontend/src/pages/home.js
@@ -14,27 +14,39 @@ function Home() {
             <div className={styles.home_page}>
                 <div className={styles.home_wrap}>
                     <div className={styles.home_box}>
-                        <h1 className={styles.head}>추천 매물</h1>
-                        <div className={styles.list_wrap}>
-                            <ListVeiw></ListVeiw>
-                        </div>
-                        <div>
-                            <a className={styles.more} onClick={goSearchResult}>추천 매물 더 보기</a>
-                        </div>
-                        <h1 className={styles.head}>인기 매물</h1>
-                        <div className={styles.rank_wrap}>
-                            <ListVeiw></ListVeiw>
-                        </div>
-                        <div>
-                            <a className={styles.more} onClick={goSearchResult}>인기 매물 더 보기</a>
-                        </div>
-                        <h1 className={styles.head}>인기 검색어</h1>
-                        <div className={styles.search_wrap}>
-
-                        </div>
-                        <div>
-                            <a className={styles.more} onClick={goSearchResult}>인기 검색어 더 보기</a>
-                        </div>
+                        <section className={styles.top_section}>
+                            <h1 className={styles.head}>추천 매물</h1>
+                            <div className={styles.list_wrap}>
+                                <ListVeiw></ListVeiw>
+                            </div>
+                            <div className={styles.more_wrap}>
+                                <a className={styles.more} onClick={goSearchResult}>추천 매물 더 보기</a>
+                            </div>
+                        </section>
+                        <section className={styles.mid_section}>
+                            <h1 className={styles.head}>인기 매물</h1>
+                            <div className={styles.rank_wrap}>
+                                <ListVeiw></ListVeiw>
+                            </div>
+                            <div className={styles.more_wrap}>
+                                <a className={styles.more} onClick={goSearchResult}>인기 매물 더 보기</a>
+                            </div>
+                        </section>
+                        <section className={styles.bottom_section}>
+                            <h1 className={styles.head}>인기 검색어</h1>
+                            <div className={styles.search_wrap}>
+                                <ul>
+                                    <li>검색어</li>
+                                    <li>검색어</li>
+                                    <li>검색어</li>
+                                    <li>검색어</li>
+                                    <li>검색어</li>
+                                </ul>
+                            </div>
+                            <div className={styles.more_wrap}>
+                                <a className={styles.more} onClick={goSearchResult}>인기 검색어 더 보기</a>
+                            </div>
+                        </section>
                     </div>
                 </div>
             </div>
