8a548ac58f4d5d8651dfa2e45eac76d8e8986e64
박민기 <86062121+mingki1242@users.noreply.github.com>
2023-05-19 00:33:03 +0900

    Add files via upload

--- src/main/java/com/example/panda/controller/WritingController.java
+++ src/main/java/com/example/panda/controller/WritingController.java
@@ -1,17 +1,44 @@
 package com.example.panda.controller;
 
+import com.example.panda.dto.WritingDTO;
+import com.example.panda.entity.WritingContent;
+import com.example.panda.entity.WritingEntity;
+import com.example.panda.service.WritingContentService;
+import com.example.panda.service.WritingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class WritingController {
 
-    @PostMapping("/api/noticeRegist")
-    public String boardwritepro(String title , String content)
+    @Autowired
+    private final WritingService writingService;
+
+    @GetMapping("/pages/noticeRegist")
+    public String boardwritingform()
     {
-        System.out.println("제목 : "+title);
-        System.out.println("제목 : "+content);
+        return "boardwrite";
+    }
+
+
+    @PostMapping("/api/noticeRegist")
+    public String boardwritepro(WritingEntity writingentity)
+    {
+        writingService.write(writingentity);
         return "";
+    }
+    @GetMapping("/api/noticeRegist")
+    public List<WritingDTO> boardList()
+    {
+        List<WritingDTO> writingDTOList = writingService.findAll();
+        return writingDTOList;
     }
 
 }
