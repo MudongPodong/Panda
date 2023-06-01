452d60307dada7c7ea97295ca244f8b089451cd0
0exia099 <0exia099@gmail.com>
2023-06-01 18:26:25 +0900

    Revert "Update WritingService.java"
    This reverts commit 0de875486a24fa8b62ff3cd7a9d7c51f809d41f2.

--- src/main/java/com/example/panda/service/WritingService.java
+++ src/main/java/com/example/panda/service/WritingService.java
@@ -1,18 +1,20 @@
 package com.example.panda.service;
 
 import com.example.panda.dto.WritingDTO;
+import com.example.panda.dto.WritingResponseDTO;
 import com.example.panda.entity.WritingEntity;
+import com.example.panda.repository.WritingDSLRepository;
 import com.example.panda.repository.WritingRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -21,12 +23,20 @@ public class WritingService {
 //    @Autowired
     private final WritingRepository writingRepository;
 
+    private final WritingDSLRepository writingDSLRepository;
+    
+    //이미지 등록 로직
+    public void saveImage(MultipartFile image , WritingEntity wee) throws IOException{
+        byte[] imageData = image.getBytes();
+        wee.setWriting_photo(imageData);
+        //writingRepository.save(wee);
+    }
+    
     public void write(WritingEntity we)
     {
         we.setFavorite_count(0);
         writingRepository.save(we);
     }
-    
     public List<WritingDTO> findAll(){
         List<WritingEntity> writingEntityList = writingRepository.findAll();
         List<WritingDTO> writingDTOList = new ArrayList<>();
@@ -38,7 +48,7 @@ public class WritingService {
         return writingDTOList;
     }
     public List<WritingDTO> findSearch(String word){
-        List<WritingEntity> writingEntityList = writingRepository.findSearch(word);
+        List<WritingEntity> writingEntityList = writingDSLRepository.findSearch(word);
         List<WritingDTO> writingDTOList = new ArrayList<>();
         for(WritingEntity writingEntity : writingEntityList) {
             writingDTOList.add(WritingDTO.toWritingDTO(writingEntity));
@@ -50,4 +60,14 @@ public class WritingService {
         WritingDTO writingDTO=WritingDTO.toWritingDTO(writingEntity.get());
         return writingDTO;
     }
+    public List<WritingResponseDTO> findPopular(){
+        List<WritingEntity> writingEntityList = writingDSLRepository.findPopularWriting();
+        List<WritingResponseDTO> writingResponseDTOList = new LinkedList<>();
+        for(WritingEntity we : writingEntityList){
+            writingResponseDTOList.add(WritingResponseDTO.toWritingResponseDTO(we, false));
+        }
+        return writingResponseDTOList;
+    }
+      
+    
 }
